5a55ba0ea8e06977b5519ab7ce97406844c91dc3
Jhonata Rodrigues <jhonata.rodrigues@brainweb.com.br>
2022-01-23 16:33:18 -0300

    chore(detox): install detox to start test e2e

---
+++ e2e/tests/listProducts.e2e.ts
@@ -0,0 +1,15 @@
+import {device, expect, element, by, waitFor} from 'detox';
+
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.text('lista de produtos'))).toBeVisible();
+  });
+});
