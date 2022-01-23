297439817e747c3a11430bafecfa0a907152279e
Jhonata Rodrigues <jhonata.rodrigues@brainweb.com.br>
2022-01-23 12:08:31 -0300

    feat(redux add and remove): redux add units and redux remove units

--- src/store/ducks/cart/index.ts
+++ src/store/ducks/cart/index.ts
@@ -10,8 +10,14 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
 
   switch (action.type) {
       case CartTypes.ADD:
+        if (state.products.find(p => p.id === action.payload.id)) {
+          return {...state, products: state.products.map(p => p.id === action.payload.id ? { ...p, quantity: p.quantity + 1 } : p)};
+        }
         return { ...state, products: [...state.products, action.payload] };
       case CartTypes.REMOVE:
+        if (state.products.find(p => p.id === action.payload && p.quantity > 1)) {
+          return {...state, products: state.products.map(p => p.id === action.payload ? { ...p, quantity: p.quantity - 1 } : p)};
+        }
         return { ...state, products: state.products.filter(product => product.id !== action.payload) };
       default:
           return state;
